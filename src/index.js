36ea2bf0324283cd69d4bc0b3154610fad98e4a0
mateosixtos <sixtosmateo@gmail.com>
2019-01-14 13:36:34 -0800

    intialize my store and pass through app component

--- src/index.js
+++ src/index.js
@@ -3,32 +3,34 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore, applyMiddleware, compose } from 'redux'
+// this comes from the package redux itself
+import { createStore } from 'redux'
+// this comes from the reducer we created
 import reducer from './reducers'
-import { Provider } from 'react-redux'
 
-const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd(action.type)
-  return result
-}
+// import { Provider } from 'react-redux'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const logger = store => next => action => {
+//   console.group(action.type)
+//   console.info('dispatching', action)
+//   let result = next(action)
+//   console.log('next state', store.getState())
+//   console.groupEnd(action.type)
+//   return result
+// }
 
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// this comes from the reducer we created
 const store = createStore(
   reducer,
-  composeEnhancers(
-    applyMiddleware(logger)
-  )
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
+console.log(store.getState())
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App store={store}/>,
   document.getElementById('root')
 )
 registerServiceWorker()
