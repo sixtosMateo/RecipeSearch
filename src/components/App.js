9eed75d56016c975a8ae310eff99fd2b79bedba7
mateosixtos <sixtosmateo@gmail.com>
2019-01-15 22:59:19 -0800

    display calendar days and mealType grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import { addRecipe, removeFromCalendar } from '../actions'
-
+import { capitalize } from '../utils/helpers'
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 
 class App extends Component {
 
@@ -22,11 +23,44 @@ class App extends Component {
   }
 
   render() {
-    console.log('Props', this.props)
+    const {calendar, remove} = this.props
+    const mealOrder = ['breakfast', 'lunch', 'dinner']
 
     return (
-      <div>
-        Hello world
+      <div className="container">
+        <ul className="meal-types">
+          {mealOrder.map((mealType) =>(
+            <li key={mealType} className="subheader">
+            {
+              capitalize(mealType)
+            }
+            </li>
+          ))}
+        </ul>
+
+        <div className='calendar'>
+          <div className='days'>
+            {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+          </div>
+          <div className='icon-grid'>
+            {calendar.map(({ day, meals }) => (
+              <ul key={day}>
+                {mealOrder.map((meal) => (
+                  <li key={meal} className='meal'>
+                    {meals[meal]
+                      ? <div className='food-item'>
+                          <img src={meals[meal].image} alt={meals[meal].label}/>
+                          <button onClick={() => remove({meal, day})}>Clear</button>
+                        </div>
+                      : <button className='icon-btn'>
+                          <CalendarIcon size={30}/>
+                        </button>}
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
      </div>
     )
   }
@@ -66,7 +100,7 @@ function mapStateToProps({calendar, food}){
       { // if these calendar on this day and this meal exist or is a thing
         meals[meal] = calendar[day][meal]
             // BECAUSE OF TOP EXAMPLE calendar[day][meal] NEEDS TO BE IN inside food[]
-            // BECAUSE IT REPRESENTS AS THE ID OF FOOD REDUCER 
+            // BECAUSE IT REPRESENTS AS THE ID OF FOOD REDUCER
           ? food[calendar[day][meal]]
           : null
         return meals
